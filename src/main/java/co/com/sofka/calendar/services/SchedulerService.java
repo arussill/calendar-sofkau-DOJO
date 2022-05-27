d9ae52ae2146cb8d6240f805b6802595cb2a36f1
Duvis07 <96325513+Duvis07@users.noreply.github.com>
2022-05-27 11:12:42 -0500

    //TODO: deben retornar un flux de programDate Flux<ProgramDate>

--- src/main/java/co/com/sofka/calendar/services/SchedulerService.java
+++ src/main/java/co/com/sofka/calendar/services/SchedulerService.java
@@ -5,6 +5,8 @@ import co.com.sofka.calendar.model.ProgramDate;
 import co.com.sofka.calendar.repositories.ProgramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -31,12 +33,13 @@ public class SchedulerService {
 
         //TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes
         //TODO: trabajar el map reactivo y no deben colectar
-        var program = programRepository.findById(programId).block();
-        return Optional.ofNullable(program)
-                .map(this::getDurationOf)
-                .orElseThrow(() -> new RuntimeException("El programa academnico no existe"))
+        var program = programRepository.findById(programId);
+        var result = program
+                .flatMapMany(programa -> Flux.fromStream(getDurationOf(programa)))
                 .map(toProgramDate(startDate, endDate, pivot[0], index))
-                .collect(Collectors.toList());
+                .switchIfEmpty(Mono.error(new RuntimeException("El programa academico no existe")));
+
+        return (List<ProgramDate>) result;
     }
 
     //No tocar
